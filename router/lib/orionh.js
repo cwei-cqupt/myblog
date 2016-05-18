1ca54de06c88e6581f9978c544ad1916f294e84b
orionwei <undefined.wei@gmail.com>
2016-05-18 13:40:04 +0800

    16.5.17

--- router/lib/orionh.js
+++ router/lib/orionh.js
@@ -1,16 +1,25 @@
 /**
  * Created by orionwei on 2016/5/17.
  */
-#!/usr/bin/env node
+//#!/usr/bin/env node
 
 var fs = require("fs");
-vra arguments = process.argv;
+var arguments = process.argv;
 if(typeof process.argv[3] === 'undefined'){
 
 }
 else{
-    render(process.argv[2],process.argv[3]);
+    read(process.argv[2],process.argv[3]);
 }
-function render(str,json){
-    console.log(str,json)
+function read(str,json){
+    fs.readFile(str,function(err, data){
+        if(err)
+            throw err;
+        else{
+            render(data,json);
+        }
+    });
+}
+function render(data,json){
+
 }
